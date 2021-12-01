c010797d6eb38fd135fc643a4ca4a732992d5d91
Iacopo02 <91663843+Iacopo02@users.noreply.github.com>
2021-12-01 15:18:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
 
+bool numero_primo(int a){
+  
+  bool output = true;
+  
+  for(int b=2; b<a; b++){
+    int risultato = a % b;
+    
+    if(risultato == 0 && b<a){
+      output = false;
+      break;
+    }
+  }
+  return output;
+}
