cac2920da579f9b0f99ce05c3ceb722d230973ca
Iacopo02 <91663843+Iacopo02@users.noreply.github.com>
2021-12-01 15:24:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+  int numero = 0;
+  
+  cin >> numero;
+  
+  if(numero_primo(numero)==true){
+    cout<<"numero primo"<<endl;
+  }else{
+    cout<<"numero non primo"<<endl;
+  }
+  
+return 0;
 }
